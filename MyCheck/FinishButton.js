05788760e0c80d79203aad391b206bde7d48b515
shykoe <57274501@qq.com>
2017-06-24 10:34:52 +0800

    6/24

--- MyCheck/FinishButton.js
+++ MyCheck/FinishButton.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import { getFormValues } from 'redux-form';
+import { asteroid } from '../asteroid';
 export class FinishButton extends Component {
+	send = ()=>{
+		asteroid.call('documents.download',{documentId:'asd'},(error, response)=>{
+			console.log(response.fileName);
+		})
+	} 
     render() {
     	const { record } = this.props;
     	var disabled = false;
-    	console.log(record);
     	record.items.forEach(
     		(item)=>{
     				if(Array.isArray(item.requirements))
@@ -15,7 +20,7 @@ export class FinishButton extends Component {
     	)
     	disabled = disabled === 1 || (record.status != 9) ? true:false;
     	return(
-    		<RaisedButton disabled={disabled} label="完成检测"/>)
+    		<RaisedButton disabled={disabled} onTouchTap={this.send} label="完成检测"/>)
     }
 }
 // FinishButton = connect(
