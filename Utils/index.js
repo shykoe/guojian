60c0c03828a6f36387ab59a353b800edc8ca15d7
shykoe <57274501@qq.com>
2017-07-02 00:24:55 +0800

    fix no Notification after  passWord modified

---
+++ Utils/index.js
@@ -0,0 +1,3 @@
+export * from './base64_blob';
+export * from './role';
+export * from './statusCode';
---
+++ Utils/statusCode.js
@@ -0,0 +1,13 @@
+export const ORDER_STATUS_UNCLAIMED = 1;
+export const ORDER_STATUS_CLAIMED = 2;
+export const ORDER_STATUS_REJECTED = 3;
+export const ORDER_STATUS_APPROVED = 4;
+export const ORDER_STATUS_CLOSED = 5;
+export const ORDER_STATUS_PAID = 6;
+export const ORDER_STATUS_PROCESSED = 7;
+export const ORDER_STATUS_SAMPLE_RECEIVED = 8;
+export const ORDER_STATUS_ASSIGNED = 9;
+export const ORDER_STATUS_TESTED = 10;
+export const ORDER_STATUS_REPORT_SHIPPED = 11;
+export const ORDER_STATUS_COMPLETED = 12;
+export const ORDER_STATUS_REFUNDED = 13
