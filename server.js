484e2cc0e4ccaca3ff4b6f7642d0112e04aed62f
Feng Qiu <q@pagerendered.in>
2017-07-25 18:37:08 +0800

    添加server.js

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.old.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const createClass = require('asteroid').createClass;
+const ws = require('ws');
+const fs = require('fs');
+const path = require('path');
 
-new WebpackDevServer(webpack(config), {
-  hot: true,
-  inline:true,
-  colors: true,
- 
-}).listen(3001, 'localhost', function(err, result) {
-  if (err) {
-    console.log(err);
+const indexFile = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(favicon(__dirname + '/static/favicon.ico'));  // eslint-disable-line
+
+app.get('/alipay/notify/', function(req, res) {
+  const Asteroid = createClass();
+  const asteroid = new Asteroid({
+    endpoint: 'ws://127.0.0.1:3333/websocket',
+    SocketConstructor: ws
+  });
+
+  asteroid.call('users.alipayConfirmPayment', req.body);
+  res.status(200).send('OK');
+});
+
+app.get('/', function (req, res) {
+  res.status(200).send(indexFile);
+});
+
+app.listen(8080, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('App started listening on port 8080');
   }
-  console.log('Listening at localhost:3001');
 });
